d0ffe4e4e3e3d55cd3f579243acac5b351c15da6
beeLAN <bilanillya@gmail.com>
2015-10-15 16:41:57 +0300

    Home. Popolnenie. EditText. Spinner

--- app/src/main/java/com/example/beelan/fiveten/activities/PayActivity.java
+++ app/src/main/java/com/example/beelan/fiveten/activities/PayActivity.java
@@ -7,19 +7,19 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 
 import com.example.beelan.fiveten.R;
-import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
+import android.support.v7.widget.AppCompatSpinner;
 
 public class PayActivity extends AppCompatActivity {
 
     public static final int ACTIVITY_PAY = R.layout.activity_pay;
     public static final int PAY_TOOLBAR = R.id.pay_toolbar;
-    public static final int IC_KEYBOARD_BACKSPACE = R.drawable.ic_keyboard_backspace;
+    public static final int IC_KEYBOARD_BACKSPACE = R.drawable.ic_arrow_left;
     public static final boolean SHOW_TITLE = false;
     private static final String[] COUNTRIES = new String[] {
             "Belgium", "France", "Italy", "Germany", "Spain"};
 
     private Toolbar toolBar;
-    private MaterialBetterSpinner paySystem;
+    private AppCompatSpinner paySystem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +34,14 @@ public class PayActivity extends AppCompatActivity {
             }
         });
 
-        initSpinner();
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
-        paySystem = (MaterialBetterSpinner)findViewById(R.id.spinner);
-        paySystem.setHint(R.string.pay_system);
-        paySystem.setUnderlineColor(getResources().getColor(R.color.colorPrimaryDark));
-
-        paySystem.setAdapter(adapter);
+//        initSpinner();
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+//        paySystem = (AppCompatSpinner)findViewById(R.id.spinner);
+////        paySystem.setHint(R.string.pay_system);
+////        paySystem.setUnderlineColor(getResources().getColor(R.color.colorPrimaryDark));
+//
+//        paySystem.setAdapter(adapter);
     }
 
 
